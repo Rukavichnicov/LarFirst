459fbd27252156a4cdbc01a4740d886914d79e2d
Sergey Rukavichnikov <serega1998love@yandex.ru>
2022-07-28 21:14:30 +0500

    Добавил контроллер категорий и базовую страницу с отображением категорий

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Blog\Admin\CategoryController;
+use App\Http\Controllers\Blog\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,15 +19,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
-    Route::resource('posts', PostController::class)->names([
-        'index' => 'blog.posts'
-    ]);
-});
-Route::resource('rest', RestTestController::class)->names('restTest');
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::group(['namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names([
+        'index' => 'blog.posts'
+    ]);
+});
+
+$groupData = [
+    'namespace' => 'App\Http\Controllers\Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    $methods = ['index', 'edit', 'store', 'update', 'create'];
+    Route::resource('categories', CategoryController::class)
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
