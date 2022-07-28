7516796409a887c89977ac8aa9843d6ffbb8c6ce
Sergey Rukavichnikov <serega1998love@yandex.ru>
2022-07-28 21:13:21 +0500

    Измениль содержание шаблона на @yield(content) и подключил стили bootstrap5

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,22 +12,25 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-8 col-sm-9 col-lg-10 content">
+                        @yield('content')
+                    </div>
                 </div>
-            </header>
+            </div>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <hr/>
+            <footer class="footer">
+                <p class="text-center">Создал Рукавичников Сергей</p>
+            </footer>
         </div>
     </body>
 </html>
