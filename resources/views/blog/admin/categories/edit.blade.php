ee1f8aa455b579320b49e734630ca37a65fa31df
Sergey Rukavichnikov <serega1998love@yandex.ru>
2022-08-02 23:20:05 +0500

    Добавил в отображение уведомление о состоянии сохранения

--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -6,6 +6,31 @@
         @method('PATCH')
         @csrf
         <div class="container">
+            @php /** @var \Illuminate\Support\ViewErrorBag $errors */ @endphp
+            @if($errors->any())
+                <div class="row justify-content-center mt-2">
+                    <div class="col-md-11">
+                        <div class="alert alert-danger" role="alert">
+                            <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true"></span>
+                            </button>
+                            {{ $errors->first() }}
+                        </div>
+                    </div>
+                </div>
+            @endif
+            @if(session('success'))
+                <div class="row justify-content-center mt-2">
+                    <div class="col-md-11">
+                        <div class="alert alert-success" role="alert">
+                            <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true"></span>
+                            </button>
+                            {{ session()->get('success') }}
+                        </div>
+                    </div>
+                </div>
+            @endif
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     @include('blog.admin.categories.includes.item_edit_main_col')
