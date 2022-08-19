440bd15bb1cb80336a75dbe7629696f0c50f316b
Sergey Rukavichnikov <serega1998love@yandex.ru>
2022-08-19 21:25:56 +0500

    Сделал отображение название родительской категории, познакомился с использованием Аксессора

--- app/Models/Blog/BlogCategory.php
+++ app/Models/Blog/BlogCategory.php
@@ -2,8 +2,11 @@
 
 namespace App\Models\Blog;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -26,4 +29,31 @@ class BlogCategory extends Model
         'parent_id',
         'description',
     ];
+    /**
+     * Получить родительскую категорию
+     * @return BlogCategory
+     */
+    public function parentCategory()
+    {
+        return $this->belongsTo(BlogCategory::class, 'parent_id', 'id');
+    }
+
+    /**
+     *
+     * @return Attribute
+     */
+    public function parentTitle(): Attribute
+    {
+        $title = $this->parentCategory->title ?? ($this->id === 1 ? 'Корень' : '???');
+        return Attribute::make(
+            get: fn () => $title,
+        );
+    }
+    /**
+     * @return BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
